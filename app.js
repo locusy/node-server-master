96517631ef2fa043aa941ea94b52b53021ad3add
tianzhi <1095098525@qq.com>
2020-03-27 14:03:02 +0800

    promise

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 console.log('nodejs...')
+
+
+function promise() {
+    const flag = false
+    return flag
+}
+// promise().then(() => { 
+//     haed()
+// })
+
+const haed = () =>{
+    console.log('seconde..')
+}
+
+
+if(promise()) {
+    setTimeout(() => {
+        console.log('time out...')
+    }, 3000)
+}else {
+    head()
+}
