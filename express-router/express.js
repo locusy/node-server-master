30b8f473056c32d316d61b437e39b8cbff8817ee
tianzhi <1095098525@qq.com>
2020-03-28 19:25:35 +0800

    express

--- express-router/express.js
+++ express-router/express.js
@@ -1,6 +1,55 @@
 /*
- * @Author: tianzhi
+ * @Author: tzz
  * @Date: 2020-03-28 13:22:15
- * @LastEditors: tianzhi
- * @LastEditTime: 2020-03-28 13:22:15
+ * @LastEditors: tzz
+ * @LastEditTime: 2020-03-28 19:25:11
+ * @Description: 基于node的http模块封装express router
  */
+const http = require('http')
+const url = require('url')
+
+let router = []
+class Application {
+    get(path, handle) {
+        if(typeof path == 'string') {
+            router.push({
+                path,
+                method: 'GET',
+                handle
+            })
+        } else {
+            router.push({
+                path: '*',
+                method: 'GET',
+                handle: path
+            })
+        }
+    }
+
+    listen(port, callback) {
+        const server = http.createServer((req, res) => {
+            const pathname = url.parse(req.url).pathname
+            for(let route of router) {
+                const { path, method, handle } = route
+                if(path == pathname && method == req.method) {
+                    handle(req, res)
+                } 
+                if(path == "*") {
+                    return handle(req, res)
+                }
+            }
+            process.on("uncaughtException", err => {
+                console.log('err:', err)
+            })
+        })
+        /**
+         * arguments: port callback
+         */
+        server.listen(...arguments)
+    }
+    
+}
+
+module.exports = function getApplication() {
+    return new Application()
+}
--- express-router/index.js
+++ express-router/index.js
@@ -1,15 +1,22 @@
 /*
- * @Author: tianzhi
+ * @Author: tzz
  * @Date: 2020-03-28 13:16:52
- * @LastEditors: tianzhi
- * @LastEditTime: 2020-03-28 18:18:34
+ * @LastEditors: tzz
+ * @LastEditTime: 2020-03-28 19:19:45
  */
-const express = require('express')
+// const express = require('express')
+const express = require('./express')
 const app = express()
 
-app.get('./', (req, res) => {
-    console.log(req, res)
+app.get('/', (req, res) => {
+    res.end('WO')
 })
+
+app.get('/users', (req, res) => {
+    res.end(JSON.stringify([{name: 'xiaoming'}]))
+})
+
+app.get({tian:12})
 
 app.listen(9999, () => {
     console.log("server is listneing at 9999...")
